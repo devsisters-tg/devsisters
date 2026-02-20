063ce71bc6e3db3eb61ccbed79d29a133b4fe444
satoru60senpai <djawada2006@gmail.com>
2026-02-20 05:34:02 +0000

    fix: dotenv optionnel + ajout dans requirements

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-
-load_dotenv()  # ← cette ligne charge ton .env
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsisters.settings')
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+    except ImportError:
+        pass
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,7 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
